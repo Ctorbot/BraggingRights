bd6f15483ca67ba18395babc3bac9ff054e5cfaa
Dylan Jordan <jordandylan52@gmail.com>
2022-09-04 20:19:43 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,tx,tsx}', './public/index.html'],
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "EBF5DF",
+        secondary: "F55D3E",
+        black: "071013",
+        razmicberry: "8A4F7D",
+        pacificblue: "08A4BD"
+      },
+    },
+  },
+  plugins: [],
+}
