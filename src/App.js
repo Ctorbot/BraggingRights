5fabba62ba0289823f2b193ecfd60834ee387933
Dylan Jordan <jordandylan52@gmail.com>
2022-09-18 23:13:43 -0500

    Mock up home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
 import * as React from "react";
-import SiteIcon from "./components/SiteIcon";
 import IconButton from "@mui/material/IconButton";
-import Paper from "@mui/material/Paper";
-import { ThemeProvider, createTheme } from '@mui/material'
+import { ThemeProvider, createTheme, Grid, Typography, Toolbar, Card, Chip, AppBar } from '@mui/material'
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 
@@ -34,12 +32,41 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Paper>
-        <SiteIcon/>
-        <IconButton sx={{ ml: 1 }} onClick={() => setDarkMode(!darkMode)} color="inherit">
-          {darkMode === true ? <Brightness7Icon /> : <Brightness4Icon />}
-        </IconButton>
-      </Paper>
+      <Grid container>
+        <AppBar position='sticky' enableColorOnDark> 
+          <Toolbar>
+            <Grid container justifyContent="flex-start" alignItems={'stretch'}>
+              <Typography>
+                Cool Logo Here
+              </Typography>
+              <Typography ml={2}>
+                Tournaments
+              </Typography>
+              <Typography ml={2}>
+                Home Rules
+              </Typography>
+            </Grid>
+            <Grid container justifyContent="flex-end">
+              <IconButton onClick={() => setDarkMode(!darkMode)} color="inherit">
+                {darkMode === true ? <Brightness7Icon /> : <Brightness4Icon />}
+              </IconButton>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </Grid>
+      <Grid container justifyContent="flex-start" alignItems="center" padding={2} spacing={2}>
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((gameRoomNumber) => (
+                <Grid xs={3} padding={2}>
+                  <Card key={gameRoomNumber} sx={{ minWidth: "stretch", height: 200 }}>
+                    <Typography>
+                      Game Room Title {gameRoomNumber}
+                    </Typography>
+                    <Chip label="Austin" />
+                    <Chip label="Dylan"/>
+                  </Card>
+                </Grid>
+              ))}
+      </Grid>
     </ThemeProvider>
   );
 }
