4400d37db27b3e234407a720f823615a385e63e1
Dylan Jordan <jordandylan52@gmail.com>
2022-10-02 21:23:59 -0500

    Create ResponsiveAppBar

---
+++ src/components/HomeRules.js
@@ -0,0 +1,10 @@
+import brlogo from '..//images/br-logo.png';
+import { AppBar, Toolbar, IconButton, Typography, Button } from '@mui/material';
+
+export default function HomeRules() {
+    return (
+        <div>
+            TEST
+        </div>
+    )
+}
